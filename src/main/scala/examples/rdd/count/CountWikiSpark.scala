7fc72088b2d8a11980aa1deeb67da763d5e139e0
Arturo Montoya <arturo.montoya332@gmail.com>
2017-12-16 10:28:26 -0800

    leverage RDD performance - distributed dataset  can be operated on in parallel then collect

--- src/main/scala/examples/rdd/count/CountWikiSpark.scala
+++ src/main/scala/examples/rdd/count/CountWikiSpark.scala
@@ -3,19 +3,20 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object CountWikiSpark {
   def main(args: Array[String]) {
-
-    val conf = new SparkConf().setAppName("wikiSparkCount").setMaster("local[*]")
+    val masterSet = "local[*]"
+    val conf = new SparkConf().setAppName("wikiSparkCount").setMaster(masterSet)
     val sc = new SparkContext(conf)
 
     val textFile = sc.textFile("in/wikiSpark.text") //: RDD[String]
     val words = textFile.flatMap(line => line.split(" ")).filter(!_.isEmpty)
-    val wordCounts = words.countByValue()
+    val wordCountsRDD = words.map(word => (word.toLowerCase, 1)).reduceByKey(_ + _)
+    val wordCounts = wordCountsRDD.collect()
 
     println("CountByValue:")
     for ((word, count) <- wordCounts) println(word + " : " + count)
     println("Count: " + words.count )
     println("Unique words Count: " + wordCounts.size )
 
-    words.saveAsTextFile("out/wikiSparkCount.text")
+    wordCountsRDD.saveAsTextFile("out/count/wikiSparkCount-" + masterSet + "-" + System.currentTimeMillis)
   }
 }
