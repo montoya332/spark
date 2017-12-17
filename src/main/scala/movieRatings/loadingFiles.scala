dcefeaaa05a11b19c148d1f0993ad4d59da2a123
Arturo Montoya <arturo.montoya332@gmail.com>
2017-12-16 22:08:34 -0800

    Load movies

--- src/main/scala/movieRatings/loadingFiles.scala
+++ src/main/scala/movieRatings/loadingFiles.scala
@@ -1,6 +1,9 @@
 package movieRatings
+import java.io.File
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.io.Source
@@ -16,8 +19,26 @@ object loadingFiles {
     val myRatings: Seq[Rating] = loadRatings("in/ml-latest-small/ratings.csv")
     val myRatingsRDD = sc.parallelize(myRatings, 1)
 
-    myRatingsRDD.saveAsTextFile("out/movieRatings/loadingFiles-" + masterSet + "-" + System.currentTimeMillis)
+    //myRatingsRDD.saveAsTextFile("out/movieRatings/loadingFiles-" + masterSet + "-" + System.currentTimeMillis)
+    val ratingsFile = sc.textFile(new File( "in/ml-latest-small/ratings.csv").toString)
+    val ratings = ratingsFile.filter(r => r!=ratingsFile.first)
+      .map { line =>
+      val fields = line.split(",")
+      // format: (timestamp % 10, Rating(userId, movieId, rating))
+      (fields(3).toLong % 10, Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble))
+    }
+
+    val moviesRDD: RDD[(Int, String)] = sc.textFile("in/ml-latest-small/movies.csv").filter(r => r!="movieId,title,genres").map { line =>
+      val fields = line.split(",")
+      // format: (movieId, movieName)
+      (fields(0).toInt, fields(1))
+    }
+    val test = moviesRDD.collect()
+   // moviesRDD.saveAsTextFile("out/movieRatings/loadingFiles-" + masterSet + "-" + System.currentTimeMillis)
+
+
   }
+
   /** Load ratings from file. */
   def loadRatings(path: String): Seq[Rating] = {
     val lines: Iterator[String] = Source.fromFile(path).getLines()
